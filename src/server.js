9d0d78b8c1e47fd958b16de247c73d7efd66c349
RenanBessa10 <bessinha92@gmail.com>
2025-12-03 22:14:43 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,3 +5,5 @@ const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Dashboard rodando em http://localhost:${PORT}`)
 })
+import analisePrecosRoutes from "./routes/analisePrecos.js";
+app.use("/api/analise-precos", analisePrecosRoutes);
